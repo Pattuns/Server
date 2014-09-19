524a04c9657fed4e47b99bcf489b016c9ef121d9
shohei <shohei@shimizu-shintaira-no-MacBook-Pro.local>
2014-09-19 21:22:18 +0900

    Sample

--- Parse.php
+++ Parse.php
@@ -6,12 +6,12 @@ define('CONSKEY','c1d257fbcb8c4ccc7065d5f4bc38442d20db87a6efc9995edbd535cfa642fd
 
 $result = file_get_contents("https://api.tokyometroapp.jp/api/v2/datapoints?rdf:type=odpt:Station&acl:consumerKey=". CONSKEY);
 $informations = json_decode($result);
-var_dump($informations);
 
 echo "<table>";
 foreach($informations as $info){
 
     $Info = get_object_vars($info);
+    var_dump($Info);
     foreach($Info as $key => $value){
 
         echo "<tr><td>" . $key . "</td><td>" . $value . "</td></tr>";
