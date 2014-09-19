c37c422e280b32f417b8f9c1bad026739d8df2bb
shohei <shohei@shimizu-shintaira-no-MacBook-Pro.local>
2014-09-19 19:59:41 +0900

    littele fix

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once '/var/www/html/metoro/sample.php';
+define('CONSKEY','c1d257fbcb8c4ccc7065d5f4bc38442d20db87a6efc9995edbd535cfa642fdf0');
+
 
 $result = file_get_contents("https://api.tokyometroapp.jp/api/v2/datapoints?rdf:type=odpt:Station&acl:consumerKey=". CONSKEY);
 $informations = json_decode($result);
