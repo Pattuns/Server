363fedcf7179405b135b7d13aa22cc9391f88b4a
Pasopeso <umegiwa@gmail.com>
2014-09-20 22:04:53 +0900

    little bit change

--- Parse.php
+++ Parse.php
@@ -5,39 +5,41 @@ define('CONSKEY','c1d257fbcb8c4ccc7065d5f4bc38442d20db87a6efc9995edbd535cfa642fd
 
 $result = file_get_contents("https://api.tokyometroapp.jp/api/v2/datapoints?rdf:type=odpt:RailwayFare&acl:consumerKey=". CONSKEY);
 $informations = json_decode($result);
-var_dump($informations);
 
 // Out Format
 //     @id | owl:sameAs | dc:title | ug:region | lon | lat
-// foreach($informations as $num => $info){
-//
-//     $Info = get_object_vars($info);
-//     foreach($Info as $key => $value){
-//         if(!is_array($value)){
-//
-//
-//             if($key == "@id")
-//                 echo $value . " | ";
-//
-//             else if($key == "owl:sameAs")
-//                 echo $value . " | ";
-//
-//             else if($key == "dc:title")
-//                 echo $value . " | ";
-//
-//             else if($key == "ug:region"){
-//                 $gisInfo = get_object_vars(json_decode(file_get_contents($value
-//                     . "?acl:consumerKey=" . CONSKEY)));
-//
-//                 echo $gisInfo["coordinates"][0] . " | ";
-//                 echo $gisInfo["coordinates"][1] . "<br />";
-//
-//             }
-//
-//         }
-//
-//     }
-//
-// }
+foreach($informations as $num => $info){
+
+    echo "<table>";
+    $Info = get_object_vars($info);
+    foreach($Info as $key => $value){
+        if(!is_array($value)){
+
+            echo "<tr><td>" . $key . "</td><td>" . $value ."</td></tr>";
+
+
+            // if($key == "@id")
+            //     echo $value . " | ";
+            //
+            // else if($key == "owl:sameAs")
+            //     echo $value . " | ";
+            //
+            // else if($key == "dc:title")
+            //     echo $value . " | ";
+            //
+            // else if($key == "ug:region"){
+            //     $gisInfo = get_object_vars(json_decode(file_get_contents($value
+            //         . "?acl:consumerKey=" . CONSKEY)));
+            //
+            //     echo $gisInfo["coordinates"][0] . " | ";
+            //     echo $gisInfo["coordinates"][1] . "<br />";
+
+        }
+
+    }
+
+
+
+}
 
 ?>
