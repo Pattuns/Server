9fcd656b0fef4b8be8021db919dd595258754880
Pasopeso <umegiwa@gmail.com>
2014-09-20 21:57:19 +0900

    little bit change

--- Parse.php
+++ Parse.php
@@ -3,40 +3,41 @@
 define('CONSKEY','c1d257fbcb8c4ccc7065d5f4bc38442d20db87a6efc9995edbd535cfa642fdf0');
 
 
-$result = file_get_contents("https://api.tokyometroapp.jp/api/v2/datapoints?rdf:type=odpt:Station&acl:consumerKey=". CONSKEY);
+$result = file_get_contents("https://api.tokyometroapp.jp/api/v2/datapoints?rdf:type=odpt:RailwayFare&acl:consumerKey=". CONSKEY);
 $informations = json_decode($result);
+var_dump($informations);
 
 // Out Format
 //     @id | owl:sameAs | dc:title | ug:region | lon | lat
-foreach($informations as $num => $info){
-
-    $Info = get_object_vars($info);
-    foreach($Info as $key => $value){
-        if(!is_array($value)){
-
-
-            if($key == "@id")
-                echo $value . " | ";
-
-            else if($key == "owl:sameAs")
-                echo $value . " | ";
-
-            else if($key == "dc:title")
-                echo $value . " | ";
-
-            else if($key == "ug:region"){
-                $gisInfo = get_object_vars(json_decode(file_get_contents($value
-                    . "?acl:consumerKey=" . CONSKEY)));
-
-                echo $gisInfo["coordinates"][0] . " | ";
-                echo $gisInfo["coordinates"][1] . "<br />";
-
-            }
-
-        }
-
-    }
-
-}
+// foreach($informations as $num => $info){
+//
+//     $Info = get_object_vars($info);
+//     foreach($Info as $key => $value){
+//         if(!is_array($value)){
+//
+//
+//             if($key == "@id")
+//                 echo $value . " | ";
+//
+//             else if($key == "owl:sameAs")
+//                 echo $value . " | ";
+//
+//             else if($key == "dc:title")
+//                 echo $value . " | ";
+//
+//             else if($key == "ug:region"){
+//                 $gisInfo = get_object_vars(json_decode(file_get_contents($value
+//                     . "?acl:consumerKey=" . CONSKEY)));
+//
+//                 echo $gisInfo["coordinates"][0] . " | ";
+//                 echo $gisInfo["coordinates"][1] . "<br />";
+//
+//             }
+//
+//         }
+//
+//     }
+//
+// }
 
 ?>
